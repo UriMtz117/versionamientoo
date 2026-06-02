9e9ff86e02496690b9951f00652b08ea61327db1
UriMtz117 <urielrm.ti22@utsjr.edu.mx>
2026-06-01 20:58:02 -0600

    Agrega páginas iniciales de integrantes

---
+++ src/app/esryil/page.js
@@ -0,0 +1,8 @@
+export default function EsryilPage() {
+  return (
+    <main>
+      <h1>Sección de Esryil</h1>
+      <p>Esta página pertenece a Esryil para la práctica de GitHub.</p>
+    </main>
+  );
+}
---
+++ src/app/milton/page.js
@@ -0,0 +1,8 @@
+export default function MiltonPage() {
+  return (
+    <main>
+      <h1>Sección de Milton</h1>
+      <p>Esta página pertenece a Milton para la práctica de GitHub.</p>
+    </main>
+  );
+}
